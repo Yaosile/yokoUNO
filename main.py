52712e24f287e878c43c42bd6a2cdb3f6765cd80
Yaosile <killer6666612345@gmail.com>
2024-10-07 15:23:23 +0200

    test

--- main.py
+++ main.py
@@ -48,15 +48,15 @@ def show_camera():
             window_handle = cv2.namedWindow(window_title, cv2.WINDOW_AUTOSIZE)
             while True:
                 ret_val, frame = video_capture.read()
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                frame = ana(frame)
+                # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                # frame = ana(frame)
 
-                display = ((255-frame + previous)*0.5).astype(np.uint8)
+                # display = ((255-frame + previous)*0.5).astype(np.uint8)
 # Check to see if the user closed the window
                 # Under GTK+ (Jetson Default), WND_PROP_VISIBLE does not work correctly. Under Qt it does
                 # GTK - Substitute WND_PROP_AUTOSIZE to detect if window has been closed by user
                 if cv2.getWindowProperty(window_title, cv2.WND_PROP_AUTOSIZE) >= 0:
-                    cv2.imshow(window_title, display)
+                    cv2.imshow(window_title, frame)
                 else:
                     break 
                 keyCode = cv2.waitKey(10) & 0xFF
