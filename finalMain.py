a5ee2fd4077af33b0f5437e48b3ac5649334f707
Yaosile <killer6666612345@gmail.com>
2024-10-21 15:32:08 +0200

    createdFinalMain

---
+++ finalMain.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=3280,
+    capture_height=2464,
+    display_width=3280,
+    display_height=2464,
+    framerate=10,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+def cameraCalibration():
+    print(gstreamer_pipeline(flip_method=0))
+
+
+if __name__ == '__main__':
+    cameraCalibration()
