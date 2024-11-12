8b35380622d4f011ff4268396aca523682f458fa
Yaosile <killer6666612345@gmail.com>
2024-11-12 23:48:07 +0200

    remove Thresholding

--- cardAcquirer.py
+++ cardAcquirer.py
@@ -25,8 +25,6 @@ def mainFootage():
                 if frame.max() != 0:
                     frame /= frame.max()
                 frame *= 255
-                frame[frame<thresh] = 0
-                frame[frame>=thresh] = 255
 
                 if cv2.getWindowProperty(boardFrame, cv2.WND_PROP_AUTOSIZE) >= 0:
                     cv2.imshow(boardFrame,frame[::2,::2,:].astype(np.uint8))
