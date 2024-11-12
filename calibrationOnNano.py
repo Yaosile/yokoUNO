502d420a5e9ca5b855789a07c6b00e32ae816519
Yaosile <killer6666612345@gmail.com>
2024-11-12 22:08:23 +0200

    idk

--- calibrationOnNano.py
+++ calibrationOnNano.py
@@ -3,18 +3,21 @@ import myOwnLibrary as myJazz
 import cv2
 
 print(myJazz.gstreamer_pipeline(flip_method=0))
-video_capture = cv2.VideoCapture(myJazz.gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
-
 frameName = 'Calibration View'
-
+video_capture = cv2.VideoCapture(myJazz.gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
 if video_capture.isOpened():
     try:
         window_handle = cv2.namedWindow(frameName, cv2.WINDOW_AUTOSIZE)
         while True:
             ret_val, frame = video_capture.read()
+
             if cv2.getWindowProperty(frameName, cv2.WND_PROP_AUTOSIZE) >= 0:
                 cv2.imshow(frameName,frame[::4,::4,:])
             else:
+                break
+
+            key = cv2.waitKey(10) & 0xFF
+            if key == ord('q'):
                 break
     finally:
         video_capture.release()
