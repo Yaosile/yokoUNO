4b5171ea2d120c1c1b22611d34bc24b4918996c6
Yaosile <killer6666612345@gmail.com>
2024-11-12 23:19:10 +0200

    testing Card Pos

--- cardAcquirer.py
+++ cardAcquirer.py
@@ -6,6 +6,9 @@ import cv2
 def mainFootage():
     boardFrame = 'Board'
     cardFrame = 'Card'
+    blur = np.ones((5,5))
+    blur /= blur.sum()
+    thresh = int(input('Enter a threshold value: '))
 
     video_capture = cv2.VideoCapture(myJazz.gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
     yuw, xuw = np.load('yMap.npy'), np.load('xMap.npy')
@@ -22,9 +25,26 @@ def mainFootage():
                 else:
                     break
 
+                if cv2.getWindowProperty(cardFrame, cv2.WND_PROP_AUTOSIZE) >= 0:
+                    cv2.imshow(cardFrame,output)
+                else:
+                    break
+
                 key = cv2.waitKey(10) & 0xFF
                 if key == ord('q'):
                     break
+
+                elif key == ord(' '):
+                    output = frame.astype(float)
+                    output = myJazz.rgb2hsv(output,Calculations='SV')
+                    output = (output[:,:,1])*output[:,:,2]*255
+                    output = myJazz.threshHold(output, thresh)
+                    output = myJazz.convolveMultiplication(output, blur)
+                    output = myJazz.threshHold(output, 254)
+                    t,b,l,r = myJazz.boundingBox(output)
+                    x,y = myJazz.midPoint(t,b,l,r)
+                    output[y,:] = 255
+                    output[:,x] = 255
         finally:
             video_capture.release()
             cv2.destroyAllWindows()
