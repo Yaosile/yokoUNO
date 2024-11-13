09b390a8222306b18c592fc5fac8af9e1fbd9250
Yaosile <killer6666612345@gmail.com>
2024-11-13 15:19:49 +0200

    see what the card sees

--- cardPosV2.py
+++ cardPosV2.py
@@ -13,11 +13,13 @@ def cardPositionFinder():
     blur = np.ones((5,5))
     blur /= blur.sum()
     boardFrame = 'Board'
+    cardFrame = 'Card'
     video_capture = cv2.VideoCapture(myJazz.gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
     yuw, xuw = np.load('yMap.npy'), np.load('xMap.npy')
     if video_capture.isOpened():
         try:
             cv2.namedWindow(boardFrame, cv2.WINDOW_AUTOSIZE)
+            cv2.namedWindow(cardFrame, cv2.WINDOW_AUTOSIZE)
             while True:
                 ret_val, frame = video_capture.read()
                 frame = frame[yuw,xuw]
@@ -27,6 +29,11 @@ def cardPositionFinder():
                 frame[frame.shape[0]//2:,:] = 0
                 if cv2.getWindowProperty(boardFrame, cv2.WND_PROP_AUTOSIZE) >= 0:
                     cv2.imshow(boardFrame,frame[::2,::2,:].astype(np.uint8))
+                else:
+                    break
+
+                if cv2.getWindowProperty(cardFrame, cv2.WND_PROP_AUTOSIZE) >= 0:
+                    cv2.imshow(cardFrame,output[::2,::2,:].astype(np.uint8))
                 else:
                     break
 
