39a2ff13800b33fac65531e72de886d5555eccc7
Yaosile <killer6666612345@gmail.com>
2024-11-13 18:38:55 +0200

    trying Arm Calibration

--- calibrationOnNanoArm.py
+++ calibrationOnNanoArm.py
@@ -1,48 +1,69 @@
 import numpy as np
+import serial
 from numpy import asanyarray as ana
 import myOwnLibrary as myJazz
 import cv2
-from PIL import Image
-import feedForwardybackwards as cnn
+
 from scipy import signal
 
-import serial
+thresh = 0
 
-
-def cardPositionFinder():
-    output = np.zeros((100,100,1))
-    cx,cy = 0,0
-    blur = np.ones((5,5))
-    blur /= blur.sum()
-    boardFrame = 'Board'
-    cardFrame = 'Card'
-    video_capture = cv2.VideoCapture(myJazz.gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+def calibrationPoints():
+    global thresh
+    thresh = int(input('Enter the threshold: '))
     yuw, xuw = np.load('yMap.npy'), np.load('xMap.npy')
+    frameName = 'Final Board Frame'
+    video_capture = cv2.VideoCapture(myJazz.gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
     if video_capture.isOpened():
         try:
-            cv2.namedWindow(boardFrame, cv2.WINDOW_AUTOSIZE)
-            cv2.namedWindow(cardFrame, cv2.WINDOW_AUTOSIZE)
+            window_handle = cv2.namedWindow(frameName, cv2.WINDOW_AUTOSIZE)
             while True:
                 ret_val, frame = video_capture.read()
                 frame = frame[yuw,xuw]
-                frame = cv2.normalize(frame, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
-                frame[cy,:] = 255
-                frame[:,cx] = 255
-                frame[frame.shape[0]//2:,:] = 0
-                if cv2.getWindowProperty(boardFrame, cv2.WND_PROP_AUTOSIZE) >= 0:
-                    cv2.imshow(boardFrame,frame[::2,::2,:].astype(np.uint8))
-                else:
-                    break
 
-                if cv2.getWindowProperty(cardFrame, cv2.WND_PROP_AUTOSIZE) >= 0:
-                    cv2.imshow(cardFrame,output[::2,::2].astype(np.uint8))
+                
+
+                if cv2.getWindowProperty(frameName, cv2.WND_PROP_AUTOSIZE) >= 0:
+                    cv2.imshow(frameName,frame[::2, ::2])
                 else:
                     break
 
                 key = cv2.waitKey(10) & 0xFF
-                if key == ord('q'):
+                if key == ord('1'):
+                    break
+        finally:
+            video_capture.release()
+            cv2.destroyAllWindows()
+    else:
+        print('Failed to open camera')
+
+def testCalibration():
+    cx,cy = 0,0
+    blur = np.ones((5,5))
+    blur = blur/blur.sum()
+    yuw, xuw = np.load('yMap.npy'), np.load('xMap.npy')
+    frameName = 'Final Board Frame'
+    video_capture = cv2.VideoCapture(myJazz.gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    if video_capture.isOpened():
+        try:
+            window_handle = cv2.namedWindow(frameName, cv2.WINDOW_AUTOSIZE)
+            while True:
+                ret_val, frame = video_capture.read()
+                frame = frame[yuw,xuw]
+                frame[cy,:] = 255
+                frame[:,cx] = 255
+                
+
+                if cv2.getWindowProperty(frameName, cv2.WND_PROP_AUTOSIZE) >= 0:
+                    cv2.imshow(frameName,frame[::2, ::2])
+                else:
                     break
 
+                frame[frame.shape[0]//2:, :] = 0
+
+                key = cv2.waitKey(10) & 0xFF
+                if key == ord('q'):
+                    break
                 elif key == ord(' '):
                     output = frame.astype(float)
                     output = myJazz.rgb2hsv(output,Calculations='SV')
@@ -60,7 +81,6 @@ def cardPositionFinder():
                     l,r = myJazz.cartesianToScara(x,y)
                     test = f'{int((l*180/np.pi + 45)*1000)} {int((r*180/np.pi + 45)*1000)} 0'
                     print(test)
-                
                 elif key == ord('m'):
                     x,y = myJazz.pixelToCartesian(cx,cy,frame.shape[1],frame.shape[0])
                     y-=15
@@ -71,24 +91,13 @@ def cardPositionFinder():
                     ser.write(test.encode())
                     ser.close()
 
-                elif key == ord('o'):
-                    test = '220000 60000 0'
-                    ser = serial.Serial('/dev/ttyUSB0', 115200)
-                    ser.write(test.encode())
-                    ser.close()
-
-                elif key == ord('d'):
-                    test = f'{int((l*180/np.pi + 45)*1000)} {int((r*180/np.pi + 45)*1000)} 1'
-                    ser = serial.Serial('/dev/ttyUSB0', 115200)
-                    ser.write(test.encode())
-                    ser.close()
-
-
-
+                    
         finally:
             video_capture.release()
             cv2.destroyAllWindows()
+    else:
+        print('Failed to open camera')
 
 if __name__ == '__main__':
-    thresh = int(input('Enter a threshold value: '))
-    cardPositionFinder()
+    calibrationPoints()
+    testCalibration()
