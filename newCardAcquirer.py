7badac90950d49382c1be6402a372094957babf8
Yaosile <killer6666612345@gmail.com>
2024-11-14 19:21:38 +0200

    test

--- newCardAcquirer.py
+++ newCardAcquirer.py
@@ -49,8 +49,6 @@ def captureCard():
                     card = card * 255
                     card, cx, cy = myJazz.isolateCard(card,card)
                     print('go')
-                frame[cy-1:cy+1, :] = 255
-                frame[:, cx-1:cx+1] = 255
         finally:
             video_capture.release()
             cv2.destroyAllWindows()
