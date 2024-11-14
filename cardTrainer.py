610e6165c40d34432cb9a39445ee089ffc0e28b2
Yaosile <killer6666612345@gmail.com>
2024-11-14 21:05:27 +0200

    canRecogniseCards

--- cardTrainer.py
+++ cardTrainer.py
@@ -45,17 +45,17 @@ for choice in files:
 #     inData.append(cnn.convolutionalSection(img))
 #     inData.append(cnn.convolutionalSection(myJazz.rot90(img,2)))
 
-weights = cnn.generateWeights(len(inData[0]), int(len(inData[0])*1.15), len(targetData[0]), 3)
+# weights = cnn.generateWeights(len(inData[0]), int(len(inData[0])*1.15), len(targetData[0]), 3)
 
 
-# weights = []
-# for i in range(2):
-#     weights.append(np.load(f'imageWeights/{i}.npy'))
-L2, weights = cnn.backProp(inData, targetData, weights, 0.01, 100, 1)
-for i in range(len(weights)):
-    np.save(f'imageWeights/{i}.npy', weights[i])
-plt.plot(L2)
-plt.show()
+weights = []
+for i in range(2):
+    weights.append(np.load(f'imageWeights/{i}.npy'))
+# L2, weights = cnn.backProp(inData, targetData, weights, 0.001, 100, 1)
+# for i in range(len(weights)):
+#     np.save(f'imageWeights/{i}.npy', weights[i])
+# plt.plot(L2)
+# plt.show()
 # # img = ana(Image.open(f'CardSnaps/b0.png'))
 # # img = myJazz.scaleImage(img, 100, 100)
 # # img = myJazz.rgb2hsv(img)
@@ -64,33 +64,14 @@ plt.show()
 # # img = [cnn.convolutionalSection(img)]
 
 # # print(lookUp[np.argmax(cnn.feedForward(img, weights))])
+guess = cnn.feedForward(inData, weights)
+tot = len(guess)
+guessSum = 0
+for i in range(len(guess)):
+    if np.argmax(guess[i]) == np.argmax(targetData[i]):
+        guessSum += 1
 
-# files = os.listdir('CardSnaps/')
-# files = [i for i in files if i[0] == 'r']
-# tot = len(files)
-# testData = []
-# correct = []
-# for f in files:
-#     img = ana(Image.open(f'CardSnaps/{f}'))[...,::-1]
-#     img = myJazz.scaleImage(img, 100, 100)
-#     img = myJazz.rgb2gray(img)
-#     # img = myJazz.rgb2hsv(img)
-#     # img = img[...,1]*img[...,2]*255
-#     img = myJazz.histogram_equalization(img)
-
-#     testData.append(cnn.convolutionalSection(img))
-
-#     output = [0]*len(lookUp)
-#     output[lookUp.index(f[1])] = 1
-#     correct.append(output)
-
-# guess = cnn.feedForward(testData, weights)
-# guessSum = 0
-# for i in range(len(guess)):
-#     if np.argmax(guess[i]) == np.argmax(correct[i]):
-#         guessSum += 1
-
-# print(guessSum, tot, guessSum/tot)
+print(guessSum, tot, guessSum/tot)
 
 
 # # ones = [i for i in files if i[1] == '1']
