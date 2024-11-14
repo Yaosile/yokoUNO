2ba718b75fc1d07247bd6d20c9bcacda7ef3994d
Yaosile <killer6666612345@gmail.com>
2024-11-14 18:46:16 +0200

    test

--- newCardAcquirer.py
+++ newCardAcquirer.py
@@ -7,41 +7,35 @@ from PIL import Image
 def captureCard():
     boardFrame = 'Board'
     cardFrame = 'Card'
-    blur = np.ones((5,5))
-    blur /= blur.sum()
-    yuw, xuw = np.load('yMap.npy'), np.load('xMap.npy')
-    card = np.zeros((100,100,3))
+    output = np.ones((100,100,3))
 
     video_capture = cv2.VideoCapture(myJazz.gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
-    # fixed_gain = 10
-    # video_capture.set(cv2.CAP_PROP_GAIN, fixed_gain)
+    yuw, xuw = np.load('yMap.npy'), np.load('xMap.npy')
     if video_capture.isOpened():
-        while True:
-            try:
-                cv2.namedWindow(boardFrame, cv2.WINDOW_AUTOSIZE)
-                # cv2.namedWindow(cardFrame, cv2.WINDOW_AUTOSIZE)
+        try:
+            cv2.namedWindow(boardFrame, cv2.WINDOW_AUTOSIZE)
+            cv2.namedWindow(cardFrame, cv2.WINDOW_AUTOSIZE)
+            while True:
                 ret_val, frame = video_capture.read()
-                # frame = frame[yuw,xuw]
+                frame = frame[yuw,xuw]
+                frame = cv2.normalize(frame, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+
                 if cv2.getWindowProperty(boardFrame, cv2.WND_PROP_AUTOSIZE) >= 0:
-                    cv2.imshow(boardFrame,frame)
+                    cv2.imshow(boardFrame,frame[::2,::2,:].astype(np.uint8))
                 else:
                     break
 
-                # if cv2.getWindowProperty(cardFrame, cv2.WND_PROP_AUTOSIZE) >= 0:
-                #     cv2.imshow(cardFrame,card.astype(np.uint8))
-                # else:
-                #     break
+                if cv2.getWindowProperty(cardFrame, cv2.WND_PROP_AUTOSIZE) >= 0:
+                    cv2.imshow(cardFrame,output.astype(np.uint8))
+                else:
+                    break
 
                 key = cv2.waitKey(10) & 0xFF
                 if key == ord('q'):
                     break
-                # elif key == ord(' '):
-                #     card = frame.copy()
-                #     card = myJazz.isolateCard(card, frame)
-
-            finally:
-                video_capture.release()
-                cv2.destroyAllWindows()
+        finally:
+            video_capture.release()
+            cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     captureCard()
