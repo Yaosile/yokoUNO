ad4a6805659f93c4f24ee4c09a56c8449fb74f5b
Yaosile <killer6666612345@gmail.com>
2025-01-09 18:19:34 +0200

    test

--- myOwnLibrary.py
+++ myOwnLibrary.py
@@ -1013,8 +1013,8 @@ def gstreamer_pipeline(
     flip_method=0,
 ):
     return (
-        
-        "nvarguscamerasrc sensor-id=%d exposuretimerange=\"13001 13001\" gainrange=\"1 1\" ! "
+        # exposuretimerange=\"13001 13001\" gainrange=\"1 1\" 
+        "nvarguscamerasrc sensor-id=%d ! "
         "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
         "nvvidconv flip-method=%d ! "
         "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
