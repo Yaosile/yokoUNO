2270c6411112bc0ce6babb6c923c1321bf7c38c5
Yaosile <killer6666612345@gmail.com>
2025-01-09 18:28:18 +0200

    test

--- myOwnLibrary.py
+++ myOwnLibrary.py
@@ -1005,10 +1005,10 @@ def distance(point1, point2):
 
 def gstreamer_pipeline(
     sensor_id=0,
-    capture_width=cameraWidth,
-    capture_height=cameraHeight,
-    display_width=cameraWidth,
-    display_height=cameraHeight,
+    capture_width=cameraWidth//4,
+    capture_height=cameraHeight//4,
+    display_width=cameraWidth//4,
+    display_height=cameraHeight//4,
     framerate=1,
     flip_method=0,
 ):
