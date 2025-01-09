e2e45f950b29361051b934ccec0743dc79c7d2b6
Yaosile <killer6666612345@gmail.com>
2025-01-09 18:47:00 +0200

    test

--- myOwnLibrary.py
+++ myOwnLibrary.py
@@ -1005,17 +1005,17 @@ def distance(point1, point2):
 
 def gstreamer_pipeline(
     sensor_id=0,
-    capture_width=cameraWidth,
-    capture_height=cameraHeight,
-    display_width=cameraWidth,
-    display_height=cameraHeight,
+    capture_width=cameraWidth//4,
+    capture_height=cameraHeight//4,
+    display_width=cameraWidth//4,
+    display_height=cameraHeight//4,
     framerate=1,
     flip_method=0,
 ):
     return (
         # exposuretimerange=\"13001 13001\" gainrange=\"1 1\" 
         "nvarguscamerasrc sensor-id=%d ! "
-        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
         "nvvidconv flip-method=%d ! "
         "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
         "videoconvert ! "
