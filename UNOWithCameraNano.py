81d05bf98837c53377a58406a5b294fe9b0120a8
Yaosile <killer6666612345@gmail.com>
2025-01-09 18:22:05 +0200

    test

--- UNOWithCameraNano.py
+++ UNOWithCameraNano.py
@@ -75,14 +75,14 @@ playerDeck = [
     'y1',
     'gs'
 ]
-cap = cv2.VideoCapture(myJazz.gstreamer_pipeline(flip_method=0, framerate=1), cv2.CAP_GSTREAMER)
+cap = cv2.VideoCapture(myJazz.gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
 # cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)  # 0.25 = Manual exposure, 0.75 = Auto exposure
 # Set manual exposure time (adjust based on your environment)
 # cap.set(cv2.CAP_PROP_EXPOSURE, -5)
 # Disable auto white balance
 # cap.set(cv2.CAP_PROP_AUTO_WB, 0)
 
-yuw, xuw = np.load('yMap.npy'), np.load('xMap.npy')
+# yuw, xuw = np.load('yMap.npy'), np.load('xMap.npy')
 
 template = myJazz.template
 oneCardWidth = template.shape[1]//13
