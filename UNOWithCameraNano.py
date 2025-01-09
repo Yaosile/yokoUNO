8cfe840912caec11646ce94f021052084be557d0
Yaosile <killer6666612345@gmail.com>
2025-01-09 19:40:43 +0200

    test

--- UNOWithCameraNano.py
+++ UNOWithCameraNano.py
@@ -75,7 +75,7 @@ playerDeck = [
     'y1',
     'gs'
 ]
-cap = cv2.VideoCapture(myJazz.gstreamer_pipeline(flip_method=0, framerate=21), cv2.CAP_GSTREAMER)
+cap = cv2.VideoCapture(myJazz.gstreamer_pipeline(flip_method=0, framerate=1), cv2.CAP_GSTREAMER)
 # cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.75)  # 0.25 = Manual exposure, 0.75 = Auto exposure
 # cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)  # 0.25 = Manual exposure, 0.75 = Auto exposure
 # # Set manual exposure time (adjust based on your environment)
