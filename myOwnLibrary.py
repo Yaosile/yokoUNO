1ed35f0f8fcdeb2d7de120bfa4efa4d64f6f00c1
Yaosile <killer6666612345@gmail.com>
2025-01-19 15:14:36 +0200

    test

--- myOwnLibrary.py
+++ myOwnLibrary.py
@@ -1016,7 +1016,7 @@ def gstreamer_pipeline(
 ):
     return (
         # 
-        'nvarguscamerasrc sensor-id=%d exposuretimerange=\"14000000 14000000\" gainrange=\"10 10\" aelock=true tnr-strength=1 ! '
+        'nvarguscamerasrc sensor-id=%d exposuretimerange=\"7400000 7400000\" gainrange=\"10 10\" aelock=true tnr-strength=1 ! '
         'video/x-raw(memory:NVMM), '
         'width=(int)%d, height=(int)%d, '
         'format=(string)NV12, framerate=(fraction)%d/1 ! '
