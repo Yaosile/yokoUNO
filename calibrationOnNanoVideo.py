42b0661df432c7eb351b250abcf106eb29790204
Yaosile <killer6666612345@gmail.com>
2025-01-19 14:58:39 +0200

    better

--- calibrationOnNanoVideo.py
+++ calibrationOnNanoVideo.py
@@ -138,7 +138,8 @@ def finalFootage():
 
 if __name__ == '__main__':
     boardSize = (int(517*3), int(605*3))
-    boardSize = (157+90,256+90)
+    boardSize = (157,256)
+    boardSize = (145*2,163*2)
     print(myJazz.gstreamer_pipeline(flip_method=0))
     rawFootage()
     snap = unDistortedFootage()
